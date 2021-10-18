0cc37366cdc093a66de3d8c433637fe56d9b95e8
fb0801 <fbhatti0801@gmail.com>
2021-10-18 11:24:01 +0100

    move the tetro left

--- app.js
+++ app.js
@@ -102,8 +102,18 @@ function freeze(){
 }
 
 
+//move the tetro lft when condition is met
+function moveLeft(){
+    undraw()
+    const isAtLeftEdge = current.some(index =>(currentPosition + index) % width ===0)
 
+    if(!isAtLeftEdge) currentPosition -=1
 
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+    currentPosition +=1
+}
+draw()
+}
 
 
 })
